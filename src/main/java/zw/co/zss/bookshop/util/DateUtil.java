5b12b91d67d9d179408cccbddd24ffc567b03374
Simbarashe Chidodo <simbachidodo1231@gmail.com>
2025-09-21 09:21:01 +0200

    SSV-01 Book Initial Project

--- src/main/java/zw/co/zss/bookshop/util/DateUtil.java
+++ src/main/java/zw/co/zss/bookshop/util/DateUtil.java
@@ -1,37 +1,24 @@
 package zw.co.zss.bookshop.util;
 
-import com.sun.org.apache.xerces.internal.jaxp.datatype.DatatypeFactoryImpl;
-
+import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-import java.util.Calendar;
+import javax.xml.datatype.DatatypeConfigurationException;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateUtil {
 
     public static XMLGregorianCalendar convertToXMLGregorianCalendar(Date date) {
-        try {
-            if(date == null) {
-                return null;
-            }
-
-            XMLGregorianCalendar xmlgc = DatatypeFactoryImpl.newInstance()
-                    .newXMLGregorianCalendar();
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            xmlgc.setYear(cal.get(Calendar.YEAR));
-            xmlgc.setMonth(cal.get(Calendar.MONTH) + 1);
-            xmlgc.setDay(cal.get(Calendar.DAY_OF_MONTH));
-            xmlgc.setHour(cal.get(Calendar.HOUR_OF_DAY));
-            xmlgc.setMinute(cal.get(Calendar.MINUTE));
-            xmlgc.setSecond(cal.get(Calendar.SECOND));
-            return xmlgc;
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-
+        if (date == null) {
+            return null;
         }
-        return null;
+        try {
+            GregorianCalendar cal = new GregorianCalendar();
+            cal.setTime(date);
+            return DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+        } catch (DatatypeConfigurationException e) {
+            throw new RuntimeException("Error while converting Date to XMLGregorianCalendar", e);
+        }
     }
+
 }
